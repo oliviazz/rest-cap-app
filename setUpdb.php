a387b51d91e5fa16afa385492ce42ddc8ae45606
Olivia Zhang <livya.zhang@gmail.com>
2016-06-06 21:20:12 -0400

    Update setUpdb.php

--- setUpdb.php
+++ setUpdb.php
@@ -7,10 +7,14 @@
     {   $servername = "restcapapp.database.windows.net";
         $user = "oliviaazhang";
         $password = "2Papayas!";
+        $pwd = "2Papayas!";
         $sqliteName = "sqlite:$dbFileName";
+        $db = 'restapp.db';
         try
         {
-            $dbh = new PDO($sqliteName, $user, $password);
+            $dbh = new PDO( "mysql:host=$servername;dbname=$db", $user, $pwd);
+		    $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            //$dbh = new PDO($sqliteName, $user, $password);
             echo('Success!');
             return $dbh;
         }
